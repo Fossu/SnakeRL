f0c4a6d917e03544ee8c719fd998b463f0b29d5a
Fossu <a.brejzek@gmail.com>
2023-04-19 11:31:28 +0200

    :)

---
+++ agent.py
@@ -0,0 +1,16 @@
+import torch
+import random
+import numpy as np
+from collections import deque   # memory storage
+from snake_game_AI import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001 # learning parameter
+
+class Agent:
+    def __init__(self):
+        return
+
+
+
